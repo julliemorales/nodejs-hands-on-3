224f6f489c1d365ff4d1f16ecca22b73c26c1c03
Jullie Angeli Morales <jullieangelimorales05@gmail.com>
2022-09-06 18:56:46 +0800

    Completed CRUD

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
+const { request } = require("express");
 const express = require("express");
 const app = express();
+const methodOverride = require("method-override");
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 // const articleRouter = require("./routes/article.routes");
-const articleMock = require("./mock/articleMock");
+const articleMockOld = require("./mock/articleMock");
+const articleMock = [...articleMockOld.articles];
 
 app.use(express.json());
 
@@ -22,43 +26,45 @@ app.get("/about", (req, res) => {
 
 // ADD Page
 app.get("/gallery/create", (req, res) => {
-  res.render("pages/add", { articles: articleMock.articles });
+  res.render("pages/add", { articles: articleMock });
 });
 
 app.post("/gallery/create", (req, res) => {
   const body = req.body;
-  console.log(body);
   res.render("pages/new", { article: body });
 });
 
 //UPDATE Page
 app.get("/gallery/:id/update", (req, res) => {
-  const article = articleMock.articles.find(
+  const article = articleMock.find(
     (article) => article.id === parseInt(req.params.id)
   );
   res.render("pages/update", { article });
 });
 
-app.put("/gallery/:id/view", (req, res) => {
-  const article = articleMock.articles.find(
-    (article) => article.id === parseInt(req.params.id)
-  );
+app.put("/gallery/:id/update", (req, res) => {
+  const article = req.body;
   res.render("pages/view", { article });
 });
 
 // DELETE Page
 app.delete("/gallery/:id/delete", (req, res) => {
-  const isExisting = articleMock.articles.some((article) => {
-    return article.id === parseInt(req.params.id);
-  });
-  isExisting
-    ? res.redirect("/gallery")
-    : res.status(404).send("Article does not exist");
+  // const isExisting = articleMock.some((article) => {
+  //   return article.id === parseInt(req.params.id);
+  // });
+  const objWithIdIndex = articleMock.findIndex(
+    (obj) => obj.id === parseInt(req.params.id)
+  );
+  articleMock.splice(objWithIdIndex, 1);
+
+  // isExisting
+  res.render("pages/gallery", { articles: articleMock });
+  // : res.status(404).send("Article does not exist");
 });
 
 // View Page
 app.get("/gallery/:id", (req, res) => {
-  const article = articleMock.articles.find(
+  const article = articleMock.find(
     (article) => article.id === parseInt(req.params.id)
   );
   res.render("pages/view", { article });
@@ -66,7 +72,7 @@ app.get("/gallery/:id", (req, res) => {
 
 // Gallery Page
 app.get("/gallery", (req, res) => {
-  res.render("pages/gallery", { articles: articleMock.articles });
+  res.render("pages/gallery", { articles: articleMock });
 });
 
 // app.use("/articles", articleRouter);

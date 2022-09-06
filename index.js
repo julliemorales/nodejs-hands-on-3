c1a9bd7691fa918fa04c445e74a819d17b21bb86
Jullie Angeli Morales <jullieangelimorales05@gmail.com>
2022-09-06 15:43:18 +0800

    Co-authored-by: mikhail-cruz

--- index.js
+++ index.js
@@ -3,36 +3,75 @@ const app = express();
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
+app.use(express.urlencoded({ extended: true }));
 
-const articleRouter = require("./routes/article.routes");
+// const articleRouter = require("./routes/article.routes");
 const articleMock = require("./mock/articleMock");
 
-// * Home Page
-app.get("/", function (req, res) {
+app.use(express.json());
+
+// Home Page
+app.get("/", (req, res) => {
   res.status(200).render("pages/index");
 });
 
-// * About Page
-app.get("/about", function (req, res) {
+// About Page
+app.get("/about", (req, res) => {
   res.render("pages/about");
 });
 
-// * View Page
-app.get("/gallery/view/:id", (req, res) => {
-  const singleArticle = articleMock.articles.find(
-    (article) => article.id === parseInt(req.params.id)
-  );
-  res.render("pages/gallery/view", { singleArticle });
+// ADD Page
+app.get("/gallery/create", (req, res) => {
+  res.render("pages/add", { articles: articleMock.articles });
 });
 
-// * Gallery Page
-app.get("/gallery", function (req, res) {
+app.post("/gallery/create", (req, res) => {
+  const body = req.body;
+  console.log(body);
+  res.render("pages/new", { article: body });
+});
+
+//UPDATE Page
+app.get("/gallery/:id/update", (req, res) => {
+  const article = articleMock.articles.find(
+    (article) => article.id === parseInt(req.params.id)
+  );
+  res.render("pages/update", { article });
+});
+
+app.put("/gallery/:id/view", (req, res) => {
+  const article = articleMock.articles.find(
+    (article) => article.id === parseInt(req.params.id)
+  );
+  res.render("pages/view", { article });
+});
+
+// DELETE Page
+app.delete("/gallery/:id/delete", (req, res) => {
+  const isExisting = articleMock.articles.some((article) => {
+    return article.id === parseInt(req.params.id);
+  });
+  isExisting
+    ? res.redirect("/gallery")
+    : res.status(404).send("Article does not exist");
+});
+
+// View Page
+app.get("/gallery/:id", (req, res) => {
+  const article = articleMock.articles.find(
+    (article) => article.id === parseInt(req.params.id)
+  );
+  res.render("pages/view", { article });
+});
+
+// Gallery Page
+app.get("/gallery", (req, res) => {
   res.render("pages/gallery", { articles: articleMock.articles });
 });
 
-app.use("/articles", articleRouter);
+// app.use("/articles", articleRouter);
 
 const PORT = 3050;
-app.listen(PORT, function () {
+app.listen(PORT, () => {
   console.log(`Server is now running on port ${PORT}`);
 });
--- mock/articleMock.js
+++ mock/articleMock.js
@@ -6,6 +6,10 @@ const articles = [
     completed: true,
     author: "Diego Laura",
     imageSrc: "https://dummyimage.com/400x300/000/fff",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit maxime animi sit, enim molestias veritatis excepturi ab aspernatur aliquam? Maxime tempora, qui distinctio eius saepe optio? Nulla reprehenderit perferendis exercitationem voluptas eveniet quas temporibus. Atque ex eveniet necessitatibus harum quos commodi, quaerat molestias voluptatibus hic sunt numquam similique eos minus eaque animi dolor possimus ipsam. Dicta culpa quis aspernatur quas ad doloremque quidem voluptatem quod. Officiis corporis asperiores expedita omnis, atque molestias? Rerum, quos dolore cupiditate provident culpa aut nisi ratione totam dignissimos beatae corporis doloribus. Dicta necessitatibus, tenetur, enim, eveniet quos facere modi expedita quia cum accusamus iste distinctio saepe dolores reprehenderit voluptate praesentium. Voluptates minus magnam officia iure corrupti iste rerum dicta cum dolorum molestias nam facilis accusantium, autem aperiam laborum recusandae, obcaecati nobis optio aut. Ut ipsum vitae natus voluptatum officiis sit inventore dicta architecto asperiores! Voluptatibus possimus accusantium nam quia quis quaerat a adipisci atque mollitia?",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit velit labore, molestiae nisi hic inventore.",
   },
   {
     id: 2,
@@ -14,6 +18,10 @@ const articles = [
     completed: true,
     author: "Laong-Laan",
     imageSrc: "https://dummyimage.com/400x300/000/fff",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit maxime animi sit, enim molestias veritatis excepturi ab aspernatur aliquam? Maxime tempora, qui distinctio eius saepe optio? Nulla reprehenderit perferendis exercitationem voluptas eveniet quas temporibus. Atque ex eveniet necessitatibus harum quos commodi, quaerat molestias voluptatibus hic sunt numquam similique eos minus eaque animi dolor possimus ipsam. Dicta culpa quis aspernatur quas ad doloremque quidem voluptatem quod. Officiis corporis asperiores expedita omnis, atque molestias? Rerum, quos dolore cupiditate provident culpa aut nisi ratione totam dignissimos beatae corporis doloribus. Dicta necessitatibus, tenetur, enim, eveniet quos facere modi expedita quia cum accusamus iste distinctio saepe dolores reprehenderit voluptate praesentium. Voluptates minus magnam officia iure corrupti iste rerum dicta cum dolorum molestias nam facilis accusantium, autem aperiam laborum recusandae, obcaecati nobis optio aut. Ut ipsum vitae natus voluptatum officiis sit inventore dicta architecto asperiores! Voluptatibus possimus accusantium nam quia quis quaerat a adipisci atque mollitia?",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit velit labore, molestiae nisi hic inventore.",
   },
   {
     id: 3,
@@ -22,6 +30,10 @@ const articles = [
     completed: true,
     author: "Agap-ito Bagumbayan",
     imageSrc: "https://dummyimage.com/400x300/000/fff",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit maxime animi sit, enim molestias veritatis excepturi ab aspernatur aliquam? Maxime tempora, qui distinctio eius saepe optio? Nulla reprehenderit perferendis exercitationem voluptas eveniet quas temporibus. Atque ex eveniet necessitatibus harum quos commodi, quaerat molestias voluptatibus hic sunt numquam similique eos minus eaque animi dolor possimus ipsam. Dicta culpa quis aspernatur quas ad doloremque quidem voluptatem quod. Officiis corporis asperiores expedita omnis, atque molestias? Rerum, quos dolore cupiditate provident culpa aut nisi ratione totam dignissimos beatae corporis doloribus. Dicta necessitatibus, tenetur, enim, eveniet quos facere modi expedita quia cum accusamus iste distinctio saepe dolores reprehenderit voluptate praesentium. Voluptates minus magnam officia iure corrupti iste rerum dicta cum dolorum molestias nam facilis accusantium, autem aperiam laborum recusandae, obcaecati nobis optio aut. Ut ipsum vitae natus voluptatum officiis sit inventore dicta architecto asperiores! Voluptatibus possimus accusantium nam quia quis quaerat a adipisci atque mollitia?",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit velit labore, molestiae nisi hic inventore.",
   },
   {
     id: 4,
@@ -30,6 +42,10 @@ const articles = [
     completed: false,
     author: "Taga-Ilog",
     imageSrc: "https://dummyimage.com/400x300/000/fff",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit maxime animi sit, enim molestias veritatis excepturi ab aspernatur aliquam? Maxime tempora, qui distinctio eius saepe optio? Nulla reprehenderit perferendis exercitationem voluptas eveniet quas temporibus. Atque ex eveniet necessitatibus harum quos commodi, quaerat molestias voluptatibus hic sunt numquam similique eos minus eaque animi dolor possimus ipsam. Dicta culpa quis aspernatur quas ad doloremque quidem voluptatem quod. Officiis corporis asperiores expedita omnis, atque molestias? Rerum, quos dolore cupiditate provident culpa aut nisi ratione totam dignissimos beatae corporis doloribus. Dicta necessitatibus, tenetur, enim, eveniet quos facere modi expedita quia cum accusamus iste distinctio saepe dolores reprehenderit voluptate praesentium. Voluptates minus magnam officia iure corrupti iste rerum dicta cum dolorum molestias nam facilis accusantium, autem aperiam laborum recusandae, obcaecati nobis optio aut. Ut ipsum vitae natus voluptatum officiis sit inventore dicta architecto asperiores! Voluptatibus possimus accusantium nam quia quis quaerat a adipisci atque mollitia?",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit velit labore, molestiae nisi hic inventore.",
   },
   {
     id: 5,
@@ -38,6 +54,70 @@ const articles = [
     completed: false,
     author: "Dimas-Ilaw",
     imageSrc: "https://dummyimage.com/400x300/000/fff",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit maxime animi sit, enim molestias veritatis excepturi ab aspernatur aliquam? Maxime tempora, qui distinctio eius saepe optio? Nulla reprehenderit perferendis exercitationem voluptas eveniet quas temporibus. Atque ex eveniet necessitatibus harum quos commodi, quaerat molestias voluptatibus hic sunt numquam similique eos minus eaque animi dolor possimus ipsam. Dicta culpa quis aspernatur quas ad doloremque quidem voluptatem quod. Officiis corporis asperiores expedita omnis, atque molestias? Rerum, quos dolore cupiditate provident culpa aut nisi ratione totam dignissimos beatae corporis doloribus. Dicta necessitatibus, tenetur, enim, eveniet quos facere modi expedita quia cum accusamus iste distinctio saepe dolores reprehenderit voluptate praesentium. Voluptates minus magnam officia iure corrupti iste rerum dicta cum dolorum molestias nam facilis accusantium, autem aperiam laborum recusandae, obcaecati nobis optio aut. Ut ipsum vitae natus voluptatum officiis sit inventore dicta architecto asperiores! Voluptatibus possimus accusantium nam quia quis quaerat a adipisci atque mollitia?",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit velit labore, molestiae nisi hic inventore.",
+  },
+  {
+    id: 6,
+    title: "Article Title",
+    order: 6,
+    completed: false,
+    author: "Dimas-Ilaw",
+    imageSrc: "https://dummyimage.com/400x300/000/fff",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit maxime animi sit, enim molestias veritatis excepturi ab aspernatur aliquam? Maxime tempora, qui distinctio eius saepe optio? Nulla reprehenderit perferendis exercitationem voluptas eveniet quas temporibus. Atque ex eveniet necessitatibus harum quos commodi, quaerat molestias voluptatibus hic sunt numquam similique eos minus eaque animi dolor possimus ipsam. Dicta culpa quis aspernatur quas ad doloremque quidem voluptatem quod. Officiis corporis asperiores expedita omnis, atque molestias? Rerum, quos dolore cupiditate provident culpa aut nisi ratione totam dignissimos beatae corporis doloribus. Dicta necessitatibus, tenetur, enim, eveniet quos facere modi expedita quia cum accusamus iste distinctio saepe dolores reprehenderit voluptate praesentium. Voluptates minus magnam officia iure corrupti iste rerum dicta cum dolorum molestias nam facilis accusantium, autem aperiam laborum recusandae, obcaecati nobis optio aut. Ut ipsum vitae natus voluptatum officiis sit inventore dicta architecto asperiores! Voluptatibus possimus accusantium nam quia quis quaerat a adipisci atque mollitia?",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit velit labore, molestiae nisi hic inventore.",
+  },
+  {
+    id: 7,
+    title: "Article Title",
+    order: 7,
+    completed: true,
+    author: "Dimas-Ilaw",
+    imageSrc: "https://dummyimage.com/400x300/000/fff",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit maxime animi sit, enim molestias veritatis excepturi ab aspernatur aliquam? Maxime tempora, qui distinctio eius saepe optio? Nulla reprehenderit perferendis exercitationem voluptas eveniet quas temporibus. Atque ex eveniet necessitatibus harum quos commodi, quaerat molestias voluptatibus hic sunt numquam similique eos minus eaque animi dolor possimus ipsam. Dicta culpa quis aspernatur quas ad doloremque quidem voluptatem quod. Officiis corporis asperiores expedita omnis, atque molestias? Rerum, quos dolore cupiditate provident culpa aut nisi ratione totam dignissimos beatae corporis doloribus. Dicta necessitatibus, tenetur, enim, eveniet quos facere modi expedita quia cum accusamus iste distinctio saepe dolores reprehenderit voluptate praesentium. Voluptates minus magnam officia iure corrupti iste rerum dicta cum dolorum molestias nam facilis accusantium, autem aperiam laborum recusandae, obcaecati nobis optio aut. Ut ipsum vitae natus voluptatum officiis sit inventore dicta architecto asperiores! Voluptatibus possimus accusantium nam quia quis quaerat a adipisci atque mollitia?",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit velit labore, molestiae nisi hic inventore.",
+  },
+  {
+    id: 8,
+    title: "Article Title",
+    order: 8,
+    completed: false,
+    author: "Dimas-Ilaw",
+    imageSrc: "https://dummyimage.com/400x300/000/fff",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit maxime animi sit, enim molestias veritatis excepturi ab aspernatur aliquam? Maxime tempora, qui distinctio eius saepe optio? Nulla reprehenderit perferendis exercitationem voluptas eveniet quas temporibus. Atque ex eveniet necessitatibus harum quos commodi, quaerat molestias voluptatibus hic sunt numquam similique eos minus eaque animi dolor possimus ipsam. Dicta culpa quis aspernatur quas ad doloremque quidem voluptatem quod. Officiis corporis asperiores expedita omnis, atque molestias? Rerum, quos dolore cupiditate provident culpa aut nisi ratione totam dignissimos beatae corporis doloribus. Dicta necessitatibus, tenetur, enim, eveniet quos facere modi expedita quia cum accusamus iste distinctio saepe dolores reprehenderit voluptate praesentium. Voluptates minus magnam officia iure corrupti iste rerum dicta cum dolorum molestias nam facilis accusantium, autem aperiam laborum recusandae, obcaecati nobis optio aut. Ut ipsum vitae natus voluptatum officiis sit inventore dicta architecto asperiores! Voluptatibus possimus accusantium nam quia quis quaerat a adipisci atque mollitia?",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit velit labore, molestiae nisi hic inventore.",
+  },
+  {
+    id: 9,
+    title: "Article Title",
+    order: 9,
+    completed: true,
+    author: "Dimas-Ilaw",
+    imageSrc: "https://dummyimage.com/400x300/000/fff",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit maxime animi sit, enim molestias veritatis excepturi ab aspernatur aliquam? Maxime tempora, qui distinctio eius saepe optio? Nulla reprehenderit perferendis exercitationem voluptas eveniet quas temporibus. Atque ex eveniet necessitatibus harum quos commodi, quaerat molestias voluptatibus hic sunt numquam similique eos minus eaque animi dolor possimus ipsam. Dicta culpa quis aspernatur quas ad doloremque quidem voluptatem quod. Officiis corporis asperiores expedita omnis, atque molestias? Rerum, quos dolore cupiditate provident culpa aut nisi ratione totam dignissimos beatae corporis doloribus. Dicta necessitatibus, tenetur, enim, eveniet quos facere modi expedita quia cum accusamus iste distinctio saepe dolores reprehenderit voluptate praesentium. Voluptates minus magnam officia iure corrupti iste rerum dicta cum dolorum molestias nam facilis accusantium, autem aperiam laborum recusandae, obcaecati nobis optio aut. Ut ipsum vitae natus voluptatum officiis sit inventore dicta architecto asperiores! Voluptatibus possimus accusantium nam quia quis quaerat a adipisci atque mollitia?",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit velit labore, molestiae nisi hic inventore.",
+  },
+  {
+    id: 10,
+    title: "Article Title",
+    order: 10,
+    completed: false,
+    author: "Dimas-Ilaw",
+    imageSrc: "https://dummyimage.com/400x300/000/fff",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit maxime animi sit, enim molestias veritatis excepturi ab aspernatur aliquam? Maxime tempora, qui distinctio eius saepe optio? Nulla reprehenderit perferendis exercitationem voluptas eveniet quas temporibus. Atque ex eveniet necessitatibus harum quos commodi, quaerat molestias voluptatibus hic sunt numquam similique eos minus eaque animi dolor possimus ipsam. Dicta culpa quis aspernatur quas ad doloremque quidem voluptatem quod. Officiis corporis asperiores expedita omnis, atque molestias? Rerum, quos dolore cupiditate provident culpa aut nisi ratione totam dignissimos beatae corporis doloribus. Dicta necessitatibus, tenetur, enim, eveniet quos facere modi expedita quia cum accusamus iste distinctio saepe dolores reprehenderit voluptate praesentium. Voluptates minus magnam officia iure corrupti iste rerum dicta cum dolorum molestias nam facilis accusantium, autem aperiam laborum recusandae, obcaecati nobis optio aut. Ut ipsum vitae natus voluptatum officiis sit inventore dicta architecto asperiores! Voluptatibus possimus accusantium nam quia quis quaerat a adipisci atque mollitia?",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit velit labore, molestiae nisi hic inventore.",
   },
 ];
 
